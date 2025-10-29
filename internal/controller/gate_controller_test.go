3b26e0b00e7af3ccb1ed71d788c973a4e65c1fbb
Robin LIORET <robin.lioret@darylsocialsoftware.com>
2025-10-29 17:25:06 +0100

    feat: basic tests ok
    Signed-off-by: Robin LIORET <robin.lioret@darylsocialsoftware.com>

--- internal/controller/gate_controller_test.go
+++ internal/controller/gate_controller_test.go
@@ -18,67 +18,116 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	gateshv1alpha1 "github.com/robinlioret/gate-operator/api/v1alpha1"
 )
 
+var testResources = []gateshv1alpha1.Gate{
+	// Simpliest gate
+	{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: gateshv1alpha1.GroupVersion.String(),
+			Kind:       "Gate",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-gate-simpliest",
+			Namespace: "default",
+		},
+		Spec: gateshv1alpha1.GateSpec{
+			Targets: []gateshv1alpha1.GateTarget{
+				{
+					Kind:       "Deployment",
+					ApiVersion: "apps/v1",
+					Name:       "coredns",
+					Namespace:  "kube-system",
+					ExistsOnly: true,
+				},
+			},
+		},
+	},
+}
+
 var _ = Describe("Gate Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+	for _, resource := range testResources {
+		Context(fmt.Sprintf("When reconciling a resource: %s", resource.Name), func() {
+			ctx := context.Background()
 
-		ctx := context.Background()
+			typeNamespacedName := types.NamespacedName{
+				Name:      resource.Name,
+				Namespace: resource.Namespace,
+			}
+			gate := &gateshv1alpha1.Gate{}
 
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		gate := &gateshv1alpha1.Gate{}
-
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind Gate")
-			err := k8sClient.Get(ctx, typeNamespacedName, gate)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &gateshv1alpha1.Gate{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
+			BeforeEach(func() {
+				By("Creating the custom resource for the Kind Gate")
+				err := k8sClient.Get(ctx, typeNamespacedName, gate)
+				if err != nil && errors.IsNotFound(err) {
+					Expect(k8sClient.Create(ctx, &resource)).To(Succeed())
 				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
-
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &gateshv1alpha1.Gate{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Cleanup the specific resource instance Gate")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &GateReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
-
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
 			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
+
+			AfterEach(func() {
+				err := k8sClient.Get(ctx, typeNamespacedName, &resource)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("Cleanup the specific resource instance Gate")
+				Expect(k8sClient.Delete(ctx, &resource)).To(Succeed())
+			})
+
+			It("should successfully reconcile the resource", func() {
+				By("Reconciling the created resource")
+				controllerReconciler := &GateReconciler{
+					Client: k8sClient,
+					Scheme: k8sClient.Scheme(),
+				}
+
+				_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+					NamespacedName: typeNamespacedName,
+				})
+				Expect(err).NotTo(HaveOccurred())
+				// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
+				// Example: If you expect a certain status condition after reconciliation, verify it here.
+
+				By("Getting the reconciled resource")
+				err = k8sClient.Get(ctx, typeNamespacedName, gate)
+				Expect(err).NotTo(HaveOccurred())
+				AddReportEntry("gate", gate)
+
+				By("Having set the status next evaluation field")
+				Expect(gate.Status.NextEvaluation.Time.After(time.Now())).To(BeTrue())
+
+				By("Having set the conditions field")
+				Expect(len(gate.Status.Conditions)).NotTo(Equal(0))
+
+				By("Having set the status state field")
+				Expect(gate.Status.State).NotTo(Equal(""))
+
+				By("Opening the gate")
+				Expect(gate.Status.State).NotTo(Equal(gateshv1alpha1.GateStateOpened))
+
+				oldNextEvaluation := gate.Status.NextEvaluation
+				By("Reconciling the created resource again (too soon)")
+				_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+					NamespacedName: typeNamespacedName,
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				By("Getting the reconciled resource")
+				err = k8sClient.Get(ctx, typeNamespacedName, gate)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("Having kept the status next evaluation field")
+				Expect(gate.Status.NextEvaluation).To(Equal(oldNextEvaluation))
+			})
 		})
-	})
+	}
 })
