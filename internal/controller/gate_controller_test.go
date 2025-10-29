b80af49daad881cc61531eddcfcd8e4fc39516e6
Robin LIORET <robin.lioret@darylsocialsoftware.com>
2025-10-29 15:34:59 +0100

    feat: review the api
    Signed-off-by: Robin LIORET <robin.lioret@darylsocialsoftware.com>

--- internal/controller/gate_controller_test.go
+++ internal/controller/gate_controller_test.go
@@ -51,7 +51,6 @@ var _ = Describe("Gate Controller", func() {
 						Name:      resourceName,
 						Namespace: "default",
 					},
-					// TODO(user): Specify other spec details if needed.
 				}
 				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
 			}
