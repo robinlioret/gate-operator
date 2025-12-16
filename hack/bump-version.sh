3d490318cbe2b44046594872f97d11865694f772
Robin LIORET <robin.lioret@darylsocialsoftware.com>
2025-12-16 17:11:10 +0100

    doc: fix install.yaml url
    Signed-off-by: Robin LIORET <robin.lioret@darylsocialsoftware.com>

--- hack/bump-version.sh
+++ hack/bump-version.sh
@@ -14,5 +14,6 @@ NEW_VERSION="$1"
 
 # Replace all occurrences of vX.Y.Z with the new version (vNEW_VERSION)
 sed -Ei "s/v[0-9]+\.[0-9]+\.[0-9]+(\-[a-zA-Z0-9\-\.]+)*/v$NEW_VERSION/g" .version
+sed -Ei "s/v[0-9]+\.[0-9]+\.[0-9]+(\-[a-zA-Z0-9\-\.]+)*\/install\.yaml/v$NEW_VERSION\/install.yaml/g" doc/docs/get-started.md
 
 echo "Bumped manifest versions to v$NEW_VERSION in docs."
